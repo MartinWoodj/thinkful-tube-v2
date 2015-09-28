18c7601bf2d2baf5d7da422846d612c812a56584
Joseph Wood <josephwood@Josephs-MacBook-Pro.local>
2015-09-28 17:42:41 -0400

    Updated code

--- apps/app.js
+++ apps/app.js
@@ -1,16 +1,42 @@
-$(document).ready(function({
+
+/*
   $(function(){
     $('query').on("submit", function(e){
       e.preventDefault();
     var request = gapi.client.youtube.search.list({
         part: "snippet",
         type: "video",
-        q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
+        q: encodeURIComponent($("#query").val()).replace(/%20/g, "+"),
         maxResults: 3,
       });
         
         
     });
   });
+*/
   
+$(document).ready(function(){
+	$('#search-button').click(function(e){
+		var search = $('#query').val()
+		console.log(search);
+		query(search);
+		});
 });
+
+function query(search){
+	$(function(){
+		  $.getJSON('https://www.googleapis.com/youtube/v3/search/?part=snippet&key=AIzaSyDyGOBDSVBpRkUfoQOhrZLPQLZtK4PNTGc&q=' + search, function(data){
+		    showResults(data);
+		    
+		  });
+		});
+	
+		function showResults(results){
+		console.log(results); 
+		$('#search-results').html('<img src="' + results.items[0].snippet.thumbnails.default.url + '">' );
+		}
+}
+
+
+
+
