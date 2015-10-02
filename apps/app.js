33433b53b495ecca62476292856473d1bf3a6339
Joseph Wood <josephwood@Josephs-MacBook-Pro.local>
2015-10-02 15:23:44 -0400

    Thumbnail arrays show as well as linked thumbs

--- apps/app.js
+++ apps/app.js
@@ -1,53 +1,44 @@
-
-/*
-  $(function(){
-    $('query').on("submit", function(e){
-      e.preventDefault();
-    var request = gapi.client.youtube.search.list({
-        part: "snippet",
-        type: "video",
-        q: encodeURIComponent($("#query").val()).replace(/%20/g, "+"),
-        maxResults: 3,
-      });
-        
-        
-    });
-  });
-*/
-  
-/*
-var myResults;
-
-for(i = 0; i < results.items.length; i++){
-    myResults = myResults + "<div> <img src='" + results.items[i].snippet.urlthing + "'></div>";
-}
-
-$('#search-container').html(myResults);
-
-*/
-
 $(document).ready(function(){
 	$('#search-button').click(function(e){
 		var search = $('#query').val()
 		console.log(search);
 		query(search);
-		});
+	});
 });
 
+
 function query(search){
-	$(function(){
-		  $.getJSON('https://www.googleapis.com/youtube/v3/search/?part=snippet&key=AIzaSyDyGOBDSVBpRkUfoQOhrZLPQLZtK4PNTGc&q=' + search, function(data){
-		    showResults(data);
-		    
-		  });
-		});
-	
-		function showResults(results){
-		console.log(results); 
-		$('#search-results').html('<img src="' + results.items[0].snippet.thumbnails.default.url + '">' );
-		}
+	$.getJSON('https://www.googleapis.com/youtube/v3/search/?part=snippet&key=AIzaSyDyGOBDSVBpRkUfoQOhrZLPQLZtK4PNTGc&q=' + search,
+	function(data){
+		showResults(data.items);
+	});
+};
+
+function showResults(results){
+	console.log(results);
+	var myResults = ''
+	for(i = 0; i < results.length; i++){
+		myResults += [
+			"<div>",
+				"<a href='http://youtube.com/watch?v=" + results[i].id.videoId +"'>",
+					"<img src='" + results[i].snippet.thumbnails.default.url + "'>",
+				"</a>",
+			"</div>"].join('');
+	}
+	console.log(myResults);
+	$('#search-results').html(myResults);
 }
 
 
+	
+/*
+var myResults;
+
+for(i = 0; i < results.items.length; i++){
+    myResults = myResults + "<div> <img src='" + results.items[i].snippet.thumbnails.default.url + "'></div>";
+}
+
+$('#search-container').html(myResults);
+*/
 
 
